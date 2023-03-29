b79d64ad4183661eb1ffd7d8ed70d1869933c96e
Pankaj <pankajsing555@gmail.com>
2023-03-29 20:48:36 +0530

    cake-project

---
+++ index.js
@@ -0,0 +1,14 @@
+function submit() {
+   let price = document.getElementsByName("price");
+   if(price[0].checked){
+    // document.getElementsByName("standard").style.Color = "red";
+}
+   else if(price[1].checked){
+    
+}
+   else if(price[2].checked){
+    
+}
+};
+submit();
+
